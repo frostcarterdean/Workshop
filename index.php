e844a87c96e03eece850906e9f069f39056b3b61
frostcarterdean <55201666+frostcarterdean@users.noreply.github.com>
2021-01-05 23:36:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -206,7 +206,7 @@
 		function fromTable($part, $id, $option) {
 			$server = "students.cah.ucf.edu";
 			$username = "ca631855";
-			$password = "Abcdef1@";
+			$password = ""; // hidden
 			$databaseName = "ca631855";
 
 			$connection = mysqli_connect("$server", "$username", "$password", "$databaseName") or die('Error');
